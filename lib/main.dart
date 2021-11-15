77fd5f223efc8494257b4e4fbae0a1cca62bf96d
ndatusung <72637595+polymath2@users.noreply.github.com>
2021-11-15 22:31:41 +0100

    removed unused code

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-//import 'package:sidehustle_capstone2_group3/src/constants/constants.dart';
+
 import 'package:sidehustle_capstone2_group3/src/views/hifi_sign_in.dart';
 import 'package:sidehustle_capstone2_group3/src/views/lofi_sign.dart';
 
